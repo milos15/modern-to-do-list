c8434eac585119d7d5a1cc6a00d81f8eda5bf94d
Milos Babic <milos.babic@creen.io>
2025-08-13 20:41:20 +0200

    Converting to React - Pushing task objects into an Array

--- react-todo/src/components/Todo.js
+++ react-todo/src/components/Todo.js
@@ -1,12 +1,70 @@
 import React from "react";
 
 const Todo = () => {
+  const[tasks, setTasks] = React.useState([]); 
+
+  // Referring to the input field
+  const refs = {
+    input: React.useRef()
+  };
+
+  // Create Task
+  const createTask = () => {
+
+    // Getting value
+    const input = refs.input.current; // Input element
+    const text = input.value; // Input value
+
+    // Preventing empty inputs
+    if (text === "") {
+      alert("Hey don't be lazy! Write something.");
+      return; 
+    }
+
+    // Pushing tasks into array
+    // Template task object
+    const task = {
+      name: text,
+      completed: false,
+      removed: false 
+    };
+
+    setTasks(previous => {
+      // Return a new array with previous objects including the last one
+      return [...previous, task]; 
+    });
+
+    // Clear the input
+    input.value = ""; 
+  
+    console.log(tasks);
+  };
+
+  const enterListener = (event) => {
+    if (event.key === "Enter") {
+      createTask();
+    }
+  };
+
+  const mouseListener = () => {
+    createTask();
+  };
+
   return (
     <div className="todo">
       <div className="todo-content">
         <h1>Let's do something today!</h1>
-        <input type="text" placeholder="Add a task" id="input" maxlength="60" />
-        <button id="button">Hit the button!</button>
+        <input
+          type="text"
+          ref={refs.input}
+          onKeyDown={enterListener}
+          placeholder="Add a task"
+          id="input"
+          maxLength="60"
+        />
+        <button id="button" onClick={mouseListener}>
+          Hit the button!
+        </button>
 
         <ul id="tasks" className="tasks"></ul>
       </div>
