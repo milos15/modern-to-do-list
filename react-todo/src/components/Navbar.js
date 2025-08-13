fc03a36ae6249e1f3a6d0e974a79b028dff48b0e
Milos Babic <milos.babic@creen.io>
2025-08-13 17:10:08 +0200

    Converting to React - Pick your Theme Feature

--- react-todo/src/components/Navbar.js
+++ react-todo/src/components/Navbar.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { useEffect } from "react";
 
 const Navbar = () => {
+  const [color, setColor] = React.useState("white");
+
+  useEffect(() => {
+    if (color === "white") {
+      document.body.classList.add("white-theme");
+      document.body.classList.remove("dark-theme");
+    } else {
+      document.body.classList.add("dark-theme");
+      document.body.classList.remove("white-theme");
+    }
+  }, [color]);
+
+  const setToBlack = () => {
+    setColor("black");
+  };
+
+  const setToWhite = () => {
+    setColor("white");
+  };
+
   return (
     <div id="navbar">
       <div id="motivational"></div>
 
       <div id="themes">
         <h3>Pick Your Theme:</h3>
-        <div className="white-theme circle" id="white-theme"></div>
-        <div className="dark-theme circle" id="dark-theme"></div>
+        <div onClick={setToWhite} className="white-theme circle" id="white-theme"></div>
+        <div onClick={setToBlack} className="dark-theme circle" id="dark-theme"></div>
       </div>
     </div>
   );
